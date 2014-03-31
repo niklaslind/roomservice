8e500317736654620b0d87097ec22d44d0ee0390
Niklas Lind <nlin@kth.se>
2014-03-31 18:54:07 +0200

    join room

--- roomservice.js
+++ roomservice.js
@@ -1,6 +1,19 @@
 app = require('express.io')()
 app.http().io()
 
-//build your realtime-web app
+
+// Setup the ready route, join room and broadcast to room.
+app.io.route('ready', function(req) {
+    req.io.join(req.data)
+    req.io.room(req.data).broadcast('announce', {
+        message: 'New client in the ' + req.data + ' room. '
+    })
+})
+
+// Send the client html.
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/client.html')
+})
 
 app.listen(7076)
+
